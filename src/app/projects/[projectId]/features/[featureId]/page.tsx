af778d87cdea236bcc3c83feb29171dac55c9c09
nahin <shariarnurnahin99@gmail.com>
2025-08-03 15:33:22 +0600

    feat: adds breadcrumb header to features page

--- src/app/projects/[projectId]/features/[featureId]/page.tsx
+++ src/app/projects/[projectId]/features/[featureId]/page.tsx
@@ -5,24 +5,37 @@ import { useGetFeature } from "@/api/features/features";
 import LoadingComponent from "@/components/common/LoadingComponent";
 import { toast } from "sonner";
 import { TicketsView } from "@/components/Tickets/TicketsView/TicketsView";
+import { SecondaryHeader } from "@/components/common/SecondaryHeader";
+import EditFeatureButton from "@/components/features/EditFeatureButton";
+import React from "react";
 
 const FeaturePage = () => {
   const projectId = useProjectId();
   const featureId = useFeatureId();
 
-  const {data: feature, isLoading, error} = useGetFeature(projectId, featureId);
+  const {
+    data: feature,
+    isLoading,
+    error,
+  } = useGetFeature(projectId, featureId);
 
   if (error) {
     toast.error(error.message);
   }
 
   if (!feature || isLoading) {
-    return <LoadingComponent/>;
+    return <LoadingComponent />;
   }
 
-  return <div>{feature.title}
-    <TicketsView hideFeatureFilter/>
-  </div>;
+  return (
+    <div>
+      <div className="flex items-center justify-between">
+        <SecondaryHeader />
+        <EditFeatureButton />
+      </div>
+      <TicketsView hideFeatureFilter />
+    </div>
+  );
 };
 
 export default FeaturePage;
